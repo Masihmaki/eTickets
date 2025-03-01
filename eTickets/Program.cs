7520c655ecfb906e45316703b85defa01f234347
masih makiabadi <masih.makiabadi@yahoo.com>
2025-03-01 13:18:03 +0330

    add view

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,11 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,5 +30,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed data
+AppDbInitializer.Seed(app);
 
 app.Run();
